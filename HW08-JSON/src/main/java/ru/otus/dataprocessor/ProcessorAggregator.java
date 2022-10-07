af61c8b61799f3ba98c8a8c74b5bf9e12dec75c5
Semen Vysokov <semenvave@yandex.ru>
2022-10-08 00:06:51 +0300

    eighth homework update

--- HW08-JSON/src/main/java/ru/otus/dataprocessor/ProcessorAggregator.java
+++ HW08-JSON/src/main/java/ru/otus/dataprocessor/ProcessorAggregator.java
@@ -2,19 +2,14 @@ package ru.otus.dataprocessor;
 
 import ru.otus.model.Measurement;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ProcessorAggregator implements Processor {
 
     @Override
     public Map<String, Double> process(List<Measurement> data) {
-        Map<String, Double> map = new HashMap<>();
-        for(Measurement measurement : data){
-            map.put(measurement.getName(),
-                    map.getOrDefault(measurement.getName(), 0.0) + measurement.getValue());
-        }
-        return map;
+        return data.stream().collect(Collectors.toMap(Measurement::getName, Measurement::getValue, Double::sum));
     }
 }
